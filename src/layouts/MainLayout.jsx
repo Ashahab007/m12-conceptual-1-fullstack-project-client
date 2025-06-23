67c0c1ae93f08990560fafdaef45e92d7ba6a3b0
Ammar Shahab <ammar.qa@generalpharma.com>
2025-06-23 13:04:46 +0600

    creating image api for choose file functionality

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 import Navbar from "../components/Shared/Navbar/Navbar";
 import Footer from "../components/Shared/Footer/Footer";
 const MainLayout = () => {
-  // 1.0 As i am going to run server side and client in one vs code so there is a import issue creates. to resolve this issue go to file => add folder to workspace =>
+  // 1.0 As i am going to run server side and client in one vs code so there is a import issue creates. to resolve this issue first type cmd to open a terminal => type "code --new-window" => go to file in vscode => add folder to workspace => select the sever => then again add folder to workspace => select the client
   return (
     <div className="bg-white">
       <Navbar />
--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,12 +1,32 @@
-import AddPlantForm from '../../../components/Form/AddPlantForm'
+import axios from "axios";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
 
 const AddPlant = () => {
+  // 2.0 my requirement is add a plant to the sever. but during upload of picture we use choose file system
+  // 2.2 place async
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form?.name?.value;
+    const category = form?.category?.value;
+    const description = form?.description?.value;
+    const price = form?.price?.value;
+    const quantity = form?.quantity?.value;
+    const image = form?.image?.files;
+    const plantData = { name, category, description, price, quantity, image };
+    console.log(plantData);
+    // 2.1 as there is a network call so we use async in 2.2 the go to network call using axios. here address will be ' "https://api.imgbb.com/1/upload?key=YOUR_CLIENT_API_KEY"' from imgBB and get the api key and set it dynamically.
+
+    const { data } = await axios.post(
+      `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`
+    );
+  };
   return (
     <div>
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm handleFormSubmit={handleFormSubmit} />
     </div>
-  )
-}
+  );
+};
 
-export default AddPlant
+export default AddPlant;
