7ef38458a569f0536f18b63545883ae4c7f6948c
Ammar Shahab <ammar.qa@generalpharma.com>
2025-06-23 12:27:14 +0600

    hudai

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 // # firebase email used ammar.pics1@gmail.com
 
-/* const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+/* .env.local
+VITE_apiKey=AIzaSyDET2zIr68cNq_kl-sfHatwkWBWke61nw8
+VITE_authDomain=plantnet-sess-conceptua-client.firebaseapp.com
+VITE_projectId=plantnet-sess-conceptua-client
+VITE_storageBucket=plantnet-sess-conceptua-client.firebasestorage.app
+VITE_messagingSenderId=729656275581
+VITE_appId=1:729656275581:web:bb79526bc861c75861371
+VITE_API_URL=http://localhost:3000
 } */
 
 const firebaseConfig = {
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,16 +1,17 @@
-import { Outlet } from 'react-router'
-import Navbar from '../components/Shared/Navbar/Navbar'
-import Footer from '../components/Shared/Footer/Footer'
+import { Outlet } from "react-router";
+import Navbar from "../components/Shared/Navbar/Navbar";
+import Footer from "../components/Shared/Footer/Footer";
 const MainLayout = () => {
+  // 1.0 As i am going to run server side and client in one vs code so there is a import issue creates. to resolve this issue go to file => add folder to workspace =>
   return (
-    <div className='bg-white'>
+    <div className="bg-white">
       <Navbar />
-      <div className='pt-24 min-h-[calc(100vh-68px)]'>
+      <div className="pt-24 min-h-[calc(100vh-68px)]">
         <Outlet />
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
