b047b8faf006fbfc57aca25792014729ab38f769
Ammar Shahab <ammar.qa@generalpharma.com>
2025-06-23 15:27:20 +0600

    image upload by choose a file functionality implemented

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -12,14 +12,35 @@ const AddPlant = () => {
     const description = form?.description?.value;
     const price = form?.price?.value;
     const quantity = form?.quantity?.value;
-    const image = form?.image?.files;
-    const plantData = { name, category, description, price, quantity, image };
-    console.log(plantData);
-    // 2.1 as there is a network call so we use async in 2.2 the go to network call using axios. here address will be ' "https://api.imgbb.com/1/upload?key=YOUR_CLIENT_API_KEY"' from imgBB and get the api key and set it dynamically.
+    // 2.3 in files apply [0]
+    const image = form?.image?.files[0];
+    // 2.4  then convert the image in a form as object
+    const imageFormData = new FormData();
+    imageFormData.append("image", image); //the name should be image as per IMGBB documentation
+
+    // 2.1 as there is a network call i.e post method so we use async in 2.2 the go to network call using axios. here address will be "https://api.imgbb.com/1/upload?key=YOUR_CLIENT_API_KEY --form "image=R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" from imgBB and get the api key and set it dynamically. Note: "--form "image=R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" this portion of api is form image data send as object.
 
     const { data } = await axios.post(
-      `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`
+      `https://api.imgbb.com/1/upload?key=${
+        import.meta.env.VITE_IMGBB_API_KEY
+      }`,
+      // 2.5 send the imageFormData as object
+      imageFormData
     );
+    console.log(data);
+    // 2.6 set in variable then send the imgURL in plantData
+    const imgUrl = data?.data?.display_url;
+
+    const plantData = {
+      name,
+      category,
+      description,
+      price,
+      quantity,
+      image: imgUrl,
+    };
+
+    console.table(plantData);
   };
   return (
     <div>
